594f2277f88b3affc7548cde575a46d637776054
JoshRicks <josh.ricks17@gmail.com>
2025-04-23 17:21:20 -0400

    added cards for clothings and itemcard module

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,7 +1,12 @@
-function ItemCard() {
+import "../blocks/ItemCard.css";
+
+function ItemCard({ item }) {
   return (
     <>
-      <div className="card"></div>
+      <div className="item-card">
+        <h3 className="item-card__name">{item.name}</h3>
+        <img className="item-card__img" src={item.link} alt={item.name} />
+      </div>
     </>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,23 @@
 import WeatherCard from "../components/WeatherCard";
 import ItemCard from "../components/ItemCard";
 import "../blocks/Main.css";
+import { defaultClothingItems } from "../utils/constants";
 
 function Main() {
   return (
     <>
       <main className="main">
-        <WeatherCard />
-        <ItemCard />
+        <section className="main__weather-card">
+          <WeatherCard />
+        </section>
+        <section className="cards">
+          <p className="main__text">Today is 75°F /You may want to wear:</p>
+          <ul className="cards__list">
+            {defaultClothingItems.map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
+          </ul>
+        </section>
       </main>
     </>
   );
