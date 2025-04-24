3db17acee59f6cc5338ac65e1041640ad60680be
JoshRicks <josh.ricks17@gmail.com>
2025-04-24 17:10:50 -0400

    added filtering to cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
+
 import "../blocks/App.css";
 import Header from "./Header.jsx";
 import Main from "./Main.jsx";
 import Footer from "./Footer.jsx";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "hot" });
+
   return (
     <>
       <div className="page">
         <div className="page__content">
           <Header />
-          <Main />
+          <Main weatherData={weatherData} />
           <Footer />
         </div>
       </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import ItemCard from "../components/ItemCard";
 import "../blocks/Main.css";
 import { defaultClothingItems } from "../utils/constants";
 
-function Main() {
+function Main({ weatherData }) {
   return (
     <>
       <main className="main">
@@ -13,9 +13,13 @@ function Main() {
         <section className="cards">
           <p className="main__text">Today is 75°F /You may want to wear:</p>
           <ul className="cards__list">
-            {defaultClothingItems.map((item) => {
-              return <ItemCard key={item._id} item={item} />;
-            })}
+            {defaultClothingItems
+              .filter((item) => {
+                return item.weather === weatherData.type;
+              })
+              .map((item) => {
+                return <ItemCard key={item._id} item={item} />;
+              })}
           </ul>
         </section>
       </main>
